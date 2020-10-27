dad6de6811481d7da16c40bd72b7a2ae1b0b6d43
Harry Dave <hdave014@gmail.com>
2020-10-28 08:57:03 +1300

    Add project files.

---
+++ refactor-this/Controllers/AccountController.cs
@@ -0,0 +1,110 @@
+﻿using refactor_this.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace refactor_this.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpGet, Route("api/Accounts/{id}")]
+        public IHttpActionResult GetById(Guid id)
+        {
+            
+            //using (var connection = Helpers.NewConnection())
+            //{
+            //    return Ok(Get(id));
+            //}
+            try
+            {
+                var account = _repository.account.GetOwnerWithDetails(id);
+                if (owner == null)
+                {
+                    _logger.LogError($"account with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInfo($"Returned account with details for id: {id}");
+
+                    var ownerResult = _mapper.Map<accountDto>(account);
+                    return Ok(accountResult);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetaccountWithDetails action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+
+        }
+
+        
+
+        [HttpGet, Route("api/Accounts")]
+        public IHttpActionResult Get()
+        {
+            using (var connection = Helpers.NewConnection())
+            {
+                SqlCommand command = new SqlCommand($"select Id from Accounts", connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                var accounts = new List<Account>();
+                while (reader.Read())
+                {
+                    var id = Guid.Parse(reader["Id"].ToString());
+                    var account = Get(id);
+                    accounts.Add(account);
+                }
+
+                return Ok(accounts);
+            }
+        }
+
+        private Account Get(Guid id)
+        {
+            using (var connection = Helpers.NewConnection())
+            {
+                SqlCommand command = new SqlCommand($"select * from Accounts where Id = '{id}'", connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                if (!reader.Read())
+                    throw new ArgumentException();
+
+                var account = new Account(id);
+                account.Name = reader["Name"].ToString();
+                account.Number = reader["Number"].ToString();
+                account.Amount = float.Parse(reader["Amount"].ToString());
+                return account;
+            }
+        }
+
+        [HttpPost, Route("api/Accounts")]
+        public IHttpActionResult Add(Account account)
+        {
+            account.Save();
+            return Ok();
+        }
+
+        [HttpPut, Route("api/Accounts/{id}")]
+        public IHttpActionResult Update(Guid id, Account account)
+        {
+            var existing = Get(id);
+            existing.Name = account.Name;
+            existing.Save();
+            return Ok();
+        }
+
+        [HttpDelete, Route("api/Accounts/{id}")]
+        public IHttpActionResult Delete(Guid id)
+        {
+            var existing = Get(id);
+            existing.Delete();
+            return Ok();
+        }
+    }
+}
---
+++ refactor-this/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace refactor_this
+{
+    public class Helpers
+    {
+        private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Harry Dave\Downloads\symtechnz-refactor-f2542f462c33\symtechnz-refactor-f2542f462c33\refactor-this\refactor-this\App_Data\refactor-this.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public static SqlConnection NewConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
